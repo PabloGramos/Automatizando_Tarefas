ecd7e8161665d33530e65d1880b4afb38f59f136
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2021-01-25 09:00:02 -0300

    atualizado
    Adicionando formato de numero brasileiro

--- PhoneNumber_ExRegulares.py
+++ PhoneNumber_ExRegulares.py
@@ -1,5 +1,9 @@
 import re
-phoneNumRegex = re.compile(r'(\(\d\d\))(\d\d\d\d\d-\d\d\d\d)')
+phoneNumRegex = re.compile(r'(\(\d\d\))?(\d\d\d\d\d-\d\d\d\d)')
 mo = phoneNumRegex.search('My number is (63)98485-7288.')
+haGerex = re.compile(r'(Ha){3,5}')
+mo1 = haGerex.search('HaHaHaHa')
 print(mo.group(1))
 print(mo.group(2))
+print(mo.groups())
+print(mo1.group())
