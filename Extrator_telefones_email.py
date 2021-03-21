92a69cece22b06e74677c64d013dd951f0bf670a
Pablo Vinícius Gonçalves Ramos <70920611+PabloGramos@users.noreply.github.com>
2021-03-20 22:25:47 -0300

    Atualizado
    Adicionado funçoes que identificam o email

--- Extrator_telefones_email.py
+++ Extrator_telefones_email.py
@@ -8,3 +8,20 @@ phoneRegex = re.compile(r"""(
     (\d{4})                         #últimos 4 dígitos 
     (\s*(ext|x|ext.)\s*(\d{2,5}))?  #extensão
 )""",re.VERBOSE)
+
+emailRegex = re.compile(r'''(
+[a-zA-Z0-9._%+-]+    #nome do usuário
+@                    #símbolo @
+[a-zA-Z0-9.-]+       #nome do domínio
+(\.[a-zA-Z]{2,4})    #ponto seguido de outros caracteres
+)''',re.VERBOSE)
+
+#Encontra as conrrespondências no texto do clipboard
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1],groups[3],groups[5]])
+    if groups[8] != '':
+        phoneNum += 'x' + groups[8]
+    matches.append(phoneNum)
+for groups
